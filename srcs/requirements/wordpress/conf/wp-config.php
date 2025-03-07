efe8d9de73c4283c6b664c8852e5dd708ed978b4
smizuoch <smizuoch@student.42tokyo.jp>
2025-03-07 14:38:55 +0900

    fix wordpress

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,41 +1,89 @@
 <?php
 /**
- * The base configuration for WordPress
+ * WordPress の基本設定
+ *
+ * このファイルは、インストール時に wp-config.php 作成ツールが利用します。
+ * ウェブサイトを作成する必要がありません。このファイルを "wp-config.php" という名前でコピーして
+ * 直接値を入力してもかまいません。
+ *
+ * このファイルには、以下の設定が含まれています：
+ *
+ * * データベース設定
+ * * 秘密鍵
+ * * データベーステーブルの接頭辞
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
  */
 
-// ** Database settings - You can get this info from your web host ** //
-define( 'DB_NAME', getenv('WORDPRESS_DB_NAME') );
-define( 'DB_USER', getenv('WORDPRESS_DB_USER') );
-define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
-define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') );
+// ** データベース設定 - こちらの情報はホスティング先から入手してください ** //
+/** WordPress のためのデータベース名 */
+
+define( 'DB_NAME', getenv('DB_NAME') );
+define( 'DB_USER', getenv('DB_USER') );
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
+define( 'DB_HOST', getenv('DB_HOST') );
+define( 'WP_HOME', getenv('WP_FULL_URL') );
+define( 'WP_SITEURL', getenv('WP_FULL_URL') );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
-// デバッグモードを有効化（troubleshooting中）
+/**#@+
+ * 認証用ユニークキーと salt
+ *
+ * これらを異なるユニークフレーズに変更してください！
+ * {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org の秘密鍵サービス} で自動生成することもできます。
+ *
+ * これらをいつでも変更して、既存のすべての cookie を無効にすることができます。
+ * これにより、すべてのユーザーは次回のログイン時に再度ログインする必要があります。
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '' );
+define( 'SECURE_AUTH_KEY',  '' );
+define( 'LOGGED_IN_KEY',    '' );
+define( 'NONCE_KEY',        '' );
+define( 'AUTH_SALT',        '' );
+define( 'SECURE_AUTH_SALT', '' );
+define( 'LOGGED_IN_SALT',   '' );
+define( 'NONCE_SALT',       '' );
+
+/**#@-*/
+
+/**
+ * WordPress データベーステーブルの接頭辞
+ *
+ * それぞれにユニークな接頭辞を与えることで一つのデータベースに複数の WordPress を
+ * インストールすることができます。半角英数字と下線のみを使用してください！
+ */
+$table_prefix = 'wp_';
+
+/**
+ * 開発者へ: WordPress デバッグモード
+ *
+ * この値を true にすると、開発中に注意 (notice) を表示します。
+ * テーマおよびプラグインの開発者には、その開発環境においてこの WP_DEBUG を
+ * 使用することを強く推奨します。
+ *
+ * その他のデバッグに利用できる定数については
+ * ドキュメンテーションをご覧ください。
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
 define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
 
-/**
- * Authentication unique keys and salts.
- */
-define('AUTH_KEY',         'unique phrase 1');
-define('SECURE_AUTH_KEY',  'unique phrase 2');
-define('LOGGED_IN_KEY',    'unique phrase 3');
-define('NONCE_KEY',        'unique phrase 4');
-define('AUTH_SALT',        'unique phrase 5');
-define('SECURE_AUTH_SALT', 'unique phrase 6');
-define('LOGGED_IN_SALT',   'unique phrase 7');
-define('NONCE_SALT',       'unique phrase 8');
+/* この行と「編集が必要なのはここまでです」の行の間にカスタム値を追加してください。 */
 
-/**
- * WordPress database table prefix.
- */
-$table_prefix = getenv('WORDPRESS_TABLE_PREFIX') ?: 'wp_';
 
-/** Absolute path to the WordPress directory. */
+
+/* 編集が必要なのはここまでです！使いやすいブログライフをお楽しみください。 */
+
+/** WordPress ディレクトリの絶対パス */
 if ( ! defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', __DIR__ . '/' );
+	define( 'ABSPATH', __DIR__ . '/' );
 }
 
-/** Sets up WordPress vars and included files. */
+/** WordPress ファイルの場所と変数をセットアップ */
 require_once ABSPATH . 'wp-settings.php';
---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# このスクリプトはビルドコンテナ内で実行されます
+# /var/www/inception/ フォルダの所有権を www-data ユーザーに変更します
+# その後、wp-config.php ファイルが /var/www/inception/ フォルダにあることを確認します
+# もしまだなければ、WordPress のコアファイルをダウンロードします
+# WordPress がインストールされていない場合はインストールします
+# 管理者ユーザーとパスワードがまだ設定されていない場合は設定します
+# これらの変数は .env ファイルで設定されています
+# 最後から2番目の行では、最も気に入った raft テーマをダウンロードして有効化します
+# 最後に、exec $@ は Dockerfile の次の CMD を実行します
+# この場合: フォアグラウンドで php-fpm7.4 サーバーを起動します
+
+# set -ex # コマンドを表示し、エラー時に終了（デバッグモード）
+
+chown -R www-data:www-data /var/www/inception/
+
+if [ ! -f "/var/www/inception/wp-config.php" ]; then
+   mv /tmp/wp-config.php /var/www/inception/
+fi
+
+sleep 10
+
+wp --allow-root --path="/var/www/inception/" core download || true
+
+if ! wp --allow-root --path="/var/www/inception/" core is-installed;
+then
+    wp  --allow-root --path="/var/www/inception/" core install \
+        --url=$WP_URL \
+        --title=$WP_TITLE \
+        --admin_user=$WP_ADMIN_USER \
+        --admin_password=$WP_ADMIN_PASSWORD \
+        --admin_email=$WP_ADMIN_EMAIL
+fi;
+
+if ! wp --allow-root --path="/var/www/inception/" user get $WP_USER;
+then
+    wp  --allow-root --path="/var/www/inception/" user create \
+        $WP_USER \
+        $WP_EMAIL \
+        --user_pass=$WP_PASSWORD \
+        --role=$WP_ROLE
+fi;
+
+wp --allow-root --path="/var/www/inception/" theme install raft --activate 
+
+exec $@
